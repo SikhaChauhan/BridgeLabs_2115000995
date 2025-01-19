baa0f6cb4a324494622866c32d905427764184e3
SikhaChauhan <csikha1010@gmail.com>
2025-01-19 18:22:39 +0530

    Average_Of_3_numbers.cs file edited in feature-19-01-2025 branch

--- Average_Of_3_numbers.cs
+++ Average_Of_3_numbers.cs
@@ -1,1 +1,23 @@
-Write a program that takes three numbers as input from the user and prints their average.
+//Q9.Write a program that takes three numbers as input from the user and prints their average.
+
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter the first number: ");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter the second number: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter the third number: ");
+        double num3 = Convert.ToDouble(Console.ReadLine());
+
+        double average = (num1 + num2 + num3) / 3;
+
+        Console.WriteLine($"The average of the three numbers is {average}");
+    }
+}
