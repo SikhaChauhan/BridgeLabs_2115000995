c4b9eb908aedbb57ac7dc1127c101c76c8c2cdee
SikhaChauhan <csikha1010@gmail.com>
2025-01-19 18:25:02 +0530

    Kms_to_miles.cs file edited in feature-19-01-2025 branch

--- Kms_to_miles.cs
+++ Kms_to_miles.cs
@@ -1,1 +1,16 @@
-Write a program that takes the distance in kilometers as input from the user and converts it into miles using the formula
+//Q10.Write a program that takes the distance in kilometers as input from the user and converts it into miles using the formula
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter the distance in kilometers: ");
+        double kilometers = Convert.ToDouble(Console.ReadLine());
+
+        double miles = kilometers * 0.621371;
+
+        Console.WriteLine($"{kilometers} kilometers is equal to {miles} miles");
+    }
+}
