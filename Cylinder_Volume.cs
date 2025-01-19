b939e4a57ca2b739d7359b3f13a66a16e583cc61
SikhaChauhan <csikha1010@gmail.com>
2025-01-19 18:12:20 +0530

    Cylinder_Volume.cs file edited in feature-19-01-2025 branch

--- Cylinder_Volume.cs
+++ Cylinder_Volume.cs
@@ -1,1 +1,20 @@
-Write a program to calculate the volume of a cylinder. Take the radius and height as inputs and use the formula
+//Q5.Write a program to calculate the volume of a cylinder. Take the radius and height as inputs and use the formula
+
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter the radius of the cylinder: ");
+        double radius = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter the height of the cylinder: ");
+        double height = Convert.ToDouble(Console.ReadLine());
+
+        double volume = Math.PI * radius * radius * height;
+
+        Console.WriteLine($"The volume of the cylinder is {volume}");
+    }
+}
