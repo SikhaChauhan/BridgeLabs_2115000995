bf3f08a3f11b89cf95b29fc6367dc27b55198ba7
SikhaChauhan <csikha1010@gmail.com>
2025-01-19 18:07:57 +0530

    Circle_Area.cs file edited in feature-19-01-2025 branch

--- Circle_Area.cs
+++ Circle_Area.cs
@@ -1,1 +1,17 @@
-Write a program to calculate the area of a circle. Take the radius as input and use the formula
+//Q4.Write a program to calculate the area of a circle. Take the radius as input and use the formula
+
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter the radius of the circle: ");
+        double radius = Convert.ToDouble(Console.ReadLine());
+
+        double area = Math.PI * radius * radius;
+
+        Console.WriteLine($"The area of the circle is {area}");
+    }
+}
