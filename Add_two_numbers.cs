f31d5d1639576351d71d78a466270f5d40c0d557
SikhaChauhan <csikha1010@gmail.com>
2025-01-19 18:01:41 +0530

    Add_two_numbers.cs file edited in feature-19-01-2025 branch

--- Add_two_numbers.cs
+++ Add_two_numbers.cs
@@ -1,2 +1,21 @@
-Write a program that takes two numbers as input from the user and prints
-their sum.
+//Q2.Write a program that takes two numbers as input from the user and prints their sum.
+
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter the first number: ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter the second number: ");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        int sum = num1 + num2;
+
+        Console.WriteLine($"The sum of {num1} and {num2} is {sum}");
+    }
+}
+
