4923f5ad5b62c39bb334d765bf44674ce84e9f25
Sikha Chauhan <106426224+SikhaChauhan@users.noreply.github.com>
2025-01-23 21:44:33 +0530

    Update HarshadNum.cs

--- Level-3 Assignment/HarshadNum.cs
+++ Level-3 Assignment/HarshadNum.cs
@@ -4,22 +4,23 @@ class HarshadNum
     static void Main()
     {
         numCheck();
-	}
-	static void numCheck(){
-		Console.Write("Enter a number: ");
+    }
+    static void numCheck(){
+	Console.Write("Enter a number: ");
         int harshad_Num = int.Parse(Console.ReadLine());
         int sum_Digits = 0;
-		int	temp = harshad_Num;
+	int temp = harshad_Num;
         while (temp != 0)
         {
             sum_Digits += temp % 10;
             temp /= 10;
         }
-        if (harshad_Num % sum_Digits == 0)
+        if (harshad_Num % sum_Digits == 0){
             Console.WriteLine($"{harshad_Num} is a Harshad Number.");
+	}
         else{
             Console.WriteLine($"{harshad_Num} is not a Harshad Number.");
-		}
 	}
+     }
 }
 
