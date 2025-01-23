4141530744b89b67cdd786c25caffe16deb1dbd9
Sikha Chauhan <106426224+SikhaChauhan@users.noreply.github.com>
2025-01-23 21:45:43 +0530

    Update AbundantNum.cs

--- Level-3 Assignment/AbundantNum.cs
+++ Level-3 Assignment/AbundantNum.cs
@@ -5,19 +5,20 @@ class AbundantNum
     {
         checkNum();
     }
-	static void checkNum(){
-		Console.Write("Enter a number: ");
+    static void checkNum(){
+	Console.Write("Enter a number: ");
         int num_Check = int.Parse(Console.ReadLine());
         int divisor_Sum = 0;
-        for (int i = 1; i < numToCheck; i++)
+        for (int i = 1; i < num_Check; i++)
         {
             if (num_Check % i == 0)
                 divisor_Sum += i;
         }
-        if (divisor_Sum > num_Check)
+        if (divisor_Sum > num_Check){
             Console.WriteLine($"{num_Check} is an Abundant Number.");
+	}
         else{
             Console.WriteLine($"{num_Check} is not an Abundant Number.");
-		}
 	}
+     }
 }
